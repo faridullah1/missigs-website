2c8392d90c5056a168d7bd8f686db62b60ebcd98
Farid ullah <faridullah996@gmail.com>
2022-09-02 22:05:25 +0500

    pricing section

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Review } from 'src/app/models/models';
+import { Review, SubscriptionType } from 'src/app/models/models';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,6 +8,7 @@ import { Review } from 'src/app/models/models';
 })
 export class LandingPageComponent {
 	reviews: Review[];
+	subscriptionTypes: SubscriptionType[];
 
 	constructor() {
 		this.reviews = [
@@ -26,7 +27,22 @@ export class LandingPageComponent {
 				rating: 5,
 				text: 'What I love about Solidgigs is they not only deliver you new leads every day, they also give you the resources you need to convert them into paying customers, all for just a fixed fee, no BS.'
 			}
-		]
+		];
+
+		this.subscriptionTypes = [
+			{
+				ammount: 21,
+				discount: 40,
+				type: 'Yearly',
+				selected: true
+			},
+			{
+				ammount: 35,
+				discount: 0,
+				type: 'Monthly',
+				selected: false
+			}
+		];
 	}
 
     rating(r: number): number[] {
