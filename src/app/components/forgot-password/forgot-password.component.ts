0c82a492777b01334400327f1fbf4d46147db4a2
Farid ullah <faridullah996@gmail.com>
2022-09-02 12:25:26 +0500

    forgot-password component.

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent implements OnInit {
+export class ForgotPasswordComponent {
+	theForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	constructor() {
+		this.theForm = new FormGroup({
+			password: new FormControl(''),
+			confirmPassword: new FormControl('')
+		});
+	}
 }
